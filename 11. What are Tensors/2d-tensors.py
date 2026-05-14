2f06ffeb40d7d796672b58c5f13fc0c8b46103bf
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-14 17:14:41 +0500

    2d tensors

--- 11. What are Tensors/2d-tensors.py
+++ 11. What are Tensors/2d-tensors.py
@@ -1,1 +1,8 @@
 #we can say that 2d tensors made of combining of different scalors and combining of different vectors
+#we can say it matrix 
+
+import numpy as np
+
+mat=np.array([[1,2,3],[4,5,6],[6,7,8]])
+
+print(mat) #output will be in form of matrix
