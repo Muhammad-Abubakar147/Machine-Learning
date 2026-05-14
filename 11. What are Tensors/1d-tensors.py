9447c1dd750ab81564b4c5a11ea401524d24e955
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-14 17:05:18 +0500

    1d tensors

--- 11. What are Tensors/1d-tensors.py
+++ 11. What are Tensors/1d-tensors.py
@@ -7,3 +7,5 @@ print(a) #output => [1 2 3 4 5 6]
 
 #This is the dimension of tensor
 print(a.ndim)
+
+# Note able thing => we can say that 1d array is 1d tensors= vector but 1d vector has 4 dimensions because 1d vector has 4 numbers
